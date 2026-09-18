50aa770d53eaac45c367c4c3842b9b4e4778122c
Ian <ghostfaceghost62@gmail.com>
2026-09-19 02:59:25 +0800

    style(admin): Enhance admin theme design with eye-appealing Lido colors and metallic gold accents

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@ if (user()) {
     $initialTheme = (string) ($themeQuery->fetchColumn() ?: '');
 }
 $isAdminShell = user() && user()['role'] === 'ADMIN';
-$assetVersion = '20260919v2';
+$assetVersion = '20260919v3';
 ?>
 <!doctype html>
 <html lang="en">
