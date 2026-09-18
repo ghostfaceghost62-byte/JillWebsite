60f39527d365de22b145f156eed201426af0dfca
Ian <ghostfaceghost62@gmail.com>
2026-09-19 01:37:10 +0800

    fix(theme): Replace tropical beach resort photo with city hotel architecture photo

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@ if (user()) {
     $initialTheme = (string) ($themeQuery->fetchColumn() ?: '');
 }
 $isAdminShell = user() && user()['role'] === 'ADMIN';
-$assetVersion = '20260913v3';
+$assetVersion = '20260919v1';
 ?>
 <!doctype html>
 <html lang="en">
